2c5dd7dbe6e7f40a00a747e4afa670dccdb0ec04
kpjablonski <kpjablonski@gmail.com>
2020-02-15 18:08:31 +0100

    Dodanieprzykładowych plików programu.

---
+++ Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                new User();
+            }
+
+            
+        }
+    }
+
+    public static class God
+    {
+        public static void Urodz(User user)
+        {
+            user.DoB = DateTime.Now;
+            Console.WriteLine(user.DoB);
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public DateTime DoB { get; set; }
+    }
+}
