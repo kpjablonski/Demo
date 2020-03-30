8b5e8dcf31dceab873aae9c9d1b3760eb71baae7
lukaszgulbinski@gmail.com <lukaszgulbinski@gmail.com>
2020-03-30 20:38:29 +0200

    cleanup

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -8,27 +8,4 @@ namespace Demo
         {
         }
     }
-
-    public static class God
-    {
-        public static void Urodz(User user)
-        {
-            user.DoB = DateTime.Now;
-            Console.WriteLine(user.DoB);
-        }
-    }
-
-    public class User
-    {
-        public string Name { get; set; }
-        public string Password { get; set; }
-        public DateTime DoB { get; set; }
-
-        sdfsdfdsf
-
-        public string Name { get; set; }
-        public string Password { get; set; }
-        public DateTime DoB { get; set; }
-
-    }
 }
