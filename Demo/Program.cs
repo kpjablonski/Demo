b7bd1bc8d695322b838280fc0b927682e238ade9
kpjablonski <kpjablonski@gmail.com>
2020-03-31 22:42:55 +0200

    Moj pierwszy kod :)

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace Demo
 {
@@ -6,6 +7,37 @@ namespace Demo
     {
         static void Main(string[] args)
         {
+            var connection = new SqlConnection();
+            connection.ConnectionString = "Server=localhost\\SQLExpress;Database=master;Trusted_Connection=True;";
+            connection.Open();
+
+            var command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "Create Database Bzp";
+            command.ExecuteNonQuery();
+
+            connection.Close();
+            connection.ConnectionString = "Server=localhost\\SQLExpress;Database=Bzp;Trusted_Connection=True;";
+
+            var createTableAdsCommand = new SqlCommand();
+            createTableAdsCommand.Connection = connection;
+            createTableAdsCommand.CommandText = 
+                @"Create Table Ads (
+                Id BIGINT PRIMARY KEY,
+                Number TEXT 
+                )
+                INSERT INTO Ads (Id, Number)
+                VALUES (3 , 'x')";
+
+            connection.Open();
+            createTableAdsCommand.ExecuteNonQuery();
+            connection.Close();
+
+            //var insertNewValues = new SqlCommand();
+            //insertNewValues.Connection = connection;
+
+
+
         }
     }
 }
