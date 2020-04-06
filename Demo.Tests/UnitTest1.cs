0967b0ae97f3d41380955ca350bf6a6b8addd5c2
kpjablonski <kpjablonski@gmail.com>
2020-04-06 22:58:15 +0200

    testy

---
+++ Demo.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Demo.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine($"Hello Grooby");
+        }
+    }
+}
