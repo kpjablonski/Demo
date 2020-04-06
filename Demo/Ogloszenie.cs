b65957cd252fdd88b98e57b1c802dadf697ace90
kpjablonski <kpjablonski@gmail.com>
2020-04-06 14:14:34 +0200

    Dodane kolumny w tabeli

---
+++ Demo/Ogloszenie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    class Ogloszenie
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public long DataPublikacji { get; set; }
+        public string Miejscowosc { get; set; }
+
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -10,22 +10,32 @@ namespace Demo
             CreateDataBaseAndTables();
 
             // insert
+            // inser mozna zastapic ad i ads klasami ktore byly tworzone
             var connection2 = new SqlConnection();
             connection2.ConnectionString = "Server=localhost\\SQLExpress;Database=Bzp;Trusted_Connection=True;";
             connection2.Open();
 
             var insertData = new SqlCommand();
             insertData.Connection = connection2;
-            insertData.CommandText =
-                @"
-                INSERT INTO Ads (Id, Number)
-                VALUES (3 , 'x')";
+            insertData.CommandText = @"
+                INSERT INTO Ads (Id, Number, DataPublikacji, Miejscowosc, Plik)
+                VALUES (3 , '510059865-N-2020', '06_04_2020', 'Wilkasy', 'Zobacz' )";
 
             insertData.ExecuteNonQuery();
             connection2.Close();
 
             //var insertNewValues = new SqlCommand();
             //insertNewValues.Connection = connection;
+
+            //BLEDY:
+            //String or binary data would be truncated
+            // Tabela byla nastepujaca:
+            // CREATE TABLE Ads(
+            //Id BIGINT PRIMARY KEY,
+            //Number TEXT,
+            //            Data_Publikacji VARCHAR(255),
+            //            Miejscowosc TEXT,
+            //            Plik VARCHAR(255),
 
 
 
@@ -40,7 +50,11 @@ namespace Demo
 
             var command = new SqlCommand();
             command.Connection = connection;
-            command.CommandText = "Create Database Bzp";
+            command.CommandText = @"
+             IF NOT EXISTS(SELECT 1 FROM sysdatabases WHERE name = 'Bzp')
+             BEGIN
+                CREATE DATABASE Bzp;
+             END";
             command.ExecuteNonQuery();
 
             connection.Close();
@@ -51,10 +65,16 @@ namespace Demo
             var createTableAdsCommand = new SqlCommand();
             createTableAdsCommand.Connection = connection;
             createTableAdsCommand.CommandText =
-                @"Create Table Ads (
-                Id BIGINT PRIMARY KEY,
-                Number TEXT 
-                )";
+               @"IF NOT EXISTS(SELECT 1 FROM sysobjects WHERE name = 'Ads' AND xtype = 'U')
+                BEGIN
+                    CREATE TABLE Ads(
+                        Id BIGINT PRIMARY KEY,
+                        Number TEXT,
+                        DataPublikacji VARCHAR(255),
+                        Miejscowosc TEXT,
+                        Plik TEXT,
+                    )
+                END";
 
 
             connection.Open();
