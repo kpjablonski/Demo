75b2e7585512eab1e20fb5795913dee9ff24f89f
Łukasz Gulbiński <lukaszgulbinski@gmail.com>
2020-04-06 23:04:59 +0200

    Initialize and Cleanup methods

--- Demo.Tests/UnitTest1.cs
+++ Demo.Tests/UnitTest1.cs
@@ -6,6 +6,18 @@ namespace Demo.Tests
     [TestClass]
     public class UnitTest1
     {
+        [TestInitialize]
+        public void Initialize()
+        {
+            // stwórz baze
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            // wywal baze
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
